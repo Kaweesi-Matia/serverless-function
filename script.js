06ded197dc52a49ee72ec058b9dd6ea08a235e8b
Kaweesi Matia <matiakaweesi@gmail.com>
2024-05-11 10:02:30 +0300

    add serverless function

--- script.js
+++ script.js
@@ -1,31 +1,61 @@
 
 
 
-// Fetch data from NASA APOD API
-fetch("https://idyllic-dodol-be9a1c.netlify.app/.netlify/functions/apodAPI") // Let's fetch multiple images
+// // Fetch data from NASA APOD API
+// fetch("https://idyllic-dodol-be9a1c.netlify.app/.netlify/functions/apodAPI") // Let's fetch multiple images
+//   .then(response => response.json())
+//   .then(data => {
+//     const apodContainer = document.getElementById("apodContainer");
+
+//     // Map over the data and create elements for each item
+//     data.map(item => {
+//       const apodItem = document.createElement("div");
+//       apodItem.classList.add("apodItem");
+
+//       const img = document.createElement("img");
+//       img.src = item.url;
+//       img.alt = "NASA APOD";
+//       img.onload = () => {
+//         img.style.display = "block"; // Show the image after it's loaded
+//       };
+
+//       const title = document.createElement("h2");
+//       title.textContent = item.title;
+
+//       apodItem.appendChild(img);
+//       apodItem.appendChild(title);
+
+//       apodContainer.appendChild(apodItem);
+//     });
+//   })
+//   .catch(error => console.error('Error fetching data:', error));
+
+
+
+
+fetch("https://idyllic-dodol-be9a1c.netlify.app/.netlify/functions/apodAPI")
   .then(response => response.json())
   .then(data => {
     const apodContainer = document.getElementById("apodContainer");
 
-    // Map over the data and create elements for each item
-    data.map(item => {
-      const apodItem = document.createElement("div");
-      apodItem.classList.add("apodItem");
+    // Create elements for the single item
+    const apodItem = document.createElement("div");
+    apodItem.classList.add("apodItem");
 
-      const img = document.createElement("img");
-      img.src = item.url;
-      img.alt = "NASA APOD";
-      img.onload = () => {
-        img.style.display = "block"; // Show the image after it's loaded
-      };
+    const img = document.createElement("img");
+    img.src = data.url;
+    img.alt = "NASA APOD";
+    img.onload = () => {
+      img.style.display = "block"; // Show the image after it's loaded
+    };
 
-      const title = document.createElement("h2");
-      title.textContent = item.title;
+    const title = document.createElement("h2");
+    title.textContent = data.title;
 
-      apodItem.appendChild(img);
-      apodItem.appendChild(title);
+    apodItem.appendChild(img);
+    apodItem.appendChild(title);
 
-      apodContainer.appendChild(apodItem);
-    });
+    apodContainer.appendChild(apodItem);
   })
   .catch(error => console.error('Error fetching data:', error));
+
